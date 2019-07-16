c1fbe229bc040df4d297c0b46850388edf5e0f53
Luiz Batanero <luizbatanero@gmail.com>
2019-07-16 16:26:10 -0300

    Persist store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -7,17 +8,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
