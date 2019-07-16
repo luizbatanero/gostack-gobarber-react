80b1f0efb4d3ae56e963bdda211fa6acf5d614d4
Luiz Batanero <luizbatanero@gmail.com>
2019-07-16 14:20:51 -0300

    Setup routes

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
+
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/profile" exact component={Profile} />
+    </Switch>
+  );
+}
